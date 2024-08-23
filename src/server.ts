429085e2355d6b777f12f561be9eb1658a7f9e23
hamiceis <hamiceis@hotmail.com>
2024-08-23 14:13:27 -0300

    feature: implematation route get activities

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import { createTrip } from "./routes/create-trip"
 import { confirmTrip } from "./routes/confim-trip"
 import { confirmParticipants } from "./routes/confirm-participant"
 import { createActivities } from "./routes/create-activities"
+import { getActivities } from "./routes/get-activities"
 
 const app = fastify()
 
@@ -16,6 +17,8 @@ app.register(createTrip)
 app.register(confirmTrip)
 app.register(confirmParticipants)
 app.register(createActivities)
+app.register(getActivities)
+
 
 app.register(cors, {
   origin: "*"
