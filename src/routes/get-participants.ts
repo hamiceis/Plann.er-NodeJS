7f07321fed8a6ab41789d301afcddc7dae734fbd
hamiceis <hamiceis@hotmail.com>
2024-08-24 10:41:29 -0300

    feat: add route get participants

---
+++ src/routes/get-participants.ts
@@ -0,0 +1,41 @@
+import { FastifyInstance } from "fastify"
+import { ZodTypeProvider } from "fastify-type-provider-zod"
+import { z } from "zod"
+import { prisma } from "../lib/prisma"
+
+
+export async function getParticipants(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get("/trips/:tripId/participants", {
+    schema: {
+      params: z.object({
+        tripId: z.string().uuid(),
+      })
+    }
+  }, async (request, reply) => {
+    const { tripId } = request.params
+
+    const trip = await prisma.trip.findUnique({
+      where: {
+        id: tripId
+      },
+      include: {
+        participants: {
+          select: {
+            id: true, 
+            email: true, 
+            name: true,
+            is_confirmed: true, 
+          }
+        }
+      }
+    })
+
+    if(!trip) {
+      throw new Error("Trip not Found")
+    }
+
+    return {
+      participants: trip.participants
+    }
+  })
+}
